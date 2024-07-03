da91c94573c7fea52548c162c634e9edb4878cf7
kkepiphane <ekoutsava@gmail.com>
2024-07-03 17:23:48 +0000

    add detect mouth

--- app.py
+++ app.py
@@ -20,19 +20,29 @@ def detectAndDisplay(frame):
             radius = int(round((w2 + h2) * 0.25))
             frame = cv.circle(frame, eye_center, radius, (255, 0, 0), 4)
 
+        mouth = mouth_cascade.detectMultiScale(faceROI, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        for (x2, y2, w2, h2) in mouth:
+            mouth_center = (x + x2 + w2 // 2, y + y2 + h2 // 2)
+            radius = int(round((w2 + h2) * 0.25))
+            frame = cv.circle(frame, mouth_center, radius, (0, 255, 0), 4)
+            break  
+        
     cv.imshow('Capture - Face detection', frame)
 
 parser = argparse.ArgumentParser(description='Code teste pour la dectection faciale.')
 parser.add_argument('--face_cascade', help='Path to face cascade.', default='data/haarcascades/haarcascade_frontalface_alt.xml')
 parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
+parser.add_argument('--mouth_cascade', help='Path to mouth cascade.', default='data/haarcascades/haarcascade_smile.xml')
 parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
 args = parser.parse_args()
 
 face_cascade_name = args.face_cascade
 eyes_cascade_name = args.eyes_cascade
+mouth_cascade_name = args.mouth_cascade
 
 face_cascade = cv.CascadeClassifier()
 eyes_cascade = cv.CascadeClassifier()
+mouth_cascade = cv.CascadeClassifier()
 
 # Load the cascades
 if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
@@ -42,7 +52,10 @@ if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
 if not eyes_cascade.load(cv.samples.findFile(eyes_cascade_name)):
     print('--(!)Error loading eyes cascade')
     exit(0)
-
+    
+if not mouth_cascade.load(cv.samples.findFile(mouth_cascade_name)):
+    print('--(!)Error loading mouth cascade')
+    exit(0)
 camera_device = args.camera
 
 # Read the video stream
